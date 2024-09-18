37deca0691d2f83584db7a6c3afaf759ef6b8999
albert1413 <albertjovani@iesmontsia.org>
2024-09-18 20:14:29 +0200

    versio 1

---
+++ Teoria.bat
@@ -0,0 +1,22 @@
+Metodes de la consola.
+
+console.assert(): Imprimeix un error a la consola si l'afirmació és falsa
+console.clear(): Netega la consola
+console.count(): Registra el nombre de vegades que s'ha cridat aquesta crida particular a count(). Aquesta funció accepta un argument etiqueta opcional
+console.error(): Imprimeix un error a la consola
+console.group(): Crea un grup de missatges a la consola
+console.groupCollapsed(): Crea un grup de missatges contraït a la consola. L'usuari haurà de desplegar-lo per veure el seu contingut
+console.groupEnd(): Tanca un grup de missatges a la consola
+console.info(): Mostra informació sobre la consola
+console.log(): Imprimeix variables i/o cadenes a la consola del navegador
+console.table(): Imprimeix informació amb format taula. Per exemple, un array
+console.time(): Inicia un cronòmetre
+console.timeEnd(): Atura el cronòmetre
+console.trace(): Imprimeix un seguiment de la pila de consola
+console.warn(): Imprimeix un missatge d'advertència a la consola
+
+
+== vs === 
+
+Igualtat (==) Verifica la igualtat de dues expressions sense tenir en compte el tipus de dada.
+Igualtat estricta (===) Fa el mateix que l'anterior, però verificant també que coincideixin els tipus de dades.
